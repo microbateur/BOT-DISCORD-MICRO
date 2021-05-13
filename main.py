1080f74515809c34abd533d396073d6bab177eeb
Vincent Cauchy <microbateur@hotmail.fr>
2021-05-13 17:50:21 +0200

    -youtube

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import discord
 from discord.ext import commands
 import random
 import time
+from discord.ext.commands.core import command
 import youtube_dl
 import asyncio
 import ffmpeg
+import emoji
 
 from discord.user import User
 
@@ -31,12 +33,12 @@ print("LAUNCH")
 @bot.event
 async def on_message(message):
 # Liste de message et d'émoji
-    Ewink = bot.get_emoji(842378741341749249)
-    print("recup wink")
+    Ewink = emoji.emojize(":wink:")
     EGulag = bot.get_emoji(799687664060203028)
     EFNews = bot.get_emoji(802274654005755935)
     AutoR = ["Mais NON!","Ah Oui " + str(Ewink) ,"Genre","Okem Cool " + str(EGulag),"Alors peut-être", "Attention " + str(EFNews)]
     print("Listed")
+    MSG = message.content
 # User
     user = [492278387038093312,683018314611687549,200232047623536640] #Id de la cible
 # Auto Message
@@ -45,80 +47,13 @@ async def on_message(message):
         CHANNEL_ID = message.channel.id
         CHANNEL = bot.get_channel(CHANNEL_ID) #CHANNEL_ID = le message dans le bon channel
         await CHANNEL.send(random.choice(AutoR)) # Message récup de la Liste de message
-        print("Emoji Send") # Vérification
+        print("Emoji Send") # Vérification  
+# Fermeture de la boucle    
+#    MSG = message.content
+#    if  MSG == "stop" in user is True :
+#        async def skip():
+#        print("break")
 #    
-
-
-###################################################
-#                    MUSIQUE                      #
-###################################################
-
-
-# Initialisation
-class Video:
-    def __init__(self, link):
-        video = ytdl.extract_info(link, download=False)
-        video_format = video["formats"][0]
-        self.url = video["webpage_url"]
-        self.stream_url = video_format["url"]
-
-# Les Commandes
-@bot.command()
-async def leave(ctx):
-    client = ctx.guild.voice_client
-    await client.disconnect()
-    musics[ctx.guild] = []
-
-@bot.command()
-async def resume(ctx):
-    client = ctx.guild.voice_client
-    if client.is_paused():
-        client.resume()
-
-
-@bot.command()
-async def pause(ctx):
-    client = ctx.guild.voice_client
-    if not client.is_paused():
-        client.pause()
-
-
-@bot.command()
-async def skip(ctx):
-    client = ctx.guild.voice_client
-    client.stop()
-
-
-def play_song(client, queue, song):
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url
-        , before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
-
-    def next(_):
-        if len(queue) > 0:
-            new_song = queue[0]
-            del queue[0]
-            play_song(client, queue, new_song)
-        else:
-            asyncio.run_coroutine_threadsafe(client.disconnect(), bot.loop)
-
-    client.play(source, after=next)
-
-
-@bot.command()
-async def play(ctx, url):
-    print("play")
-    client = ctx.guild.voice_client
-
-    if client and client.channel:
-        video = Video(url)
-        musics[ctx.guild].append(video)
-    else:
-        channel = ctx.author.voice.channel
-        video = Video(url)
-        musics[ctx.guild] = []
-        client = await channel.connect()
-        await ctx.send("Je lance : {video.url}")
-        play_song(client, musics[ctx.guild], video)
 
 
 ###################################################
